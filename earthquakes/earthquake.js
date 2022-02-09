7ccff38ad92454a68971941fd8935330d7a59ee8
Kevin Kuei <kevinkuei@gmail.com>
2022-02-08 18:17:40 -0800

    Table results, mag by size and color

--- earthquakes/earthquake.js
+++ earthquakes/earthquake.js
@@ -1,61 +1,108 @@
+// be kinda cool to combine canadian API with US
+// https://earthquakescanada.nrcan.gc.ca/stndon/NEDB-BNDS/bulletin-en.php?display=1&endtime=2022-02-09T23%3A59%3A59&eventtype=L&latitude=63.13&limit=4001&longitude=-90.34&maxdepth=1000&maxlatitude=90&maxlongitude=180&maxmagnitude=10&maxradius=45.04504504504504&mindepth=-5&minlatitude=-90&minlongitude=-180&minmagnitude=-5&minradius=0&onlyfelt=0&starttime=2022-01-09T00%3A00%3A00
+// add a table of summary statistics and earthquake metatable as well
+// Magnitude <2 2 3 4 5 >=6 || w/ totals...
+
 // Need a function to update plot when field updated, or onclick
 
 // Need get javascript DOM elements
 
-const minMagVal = document.getElementByID("minMagBox")
-const maxMagVal = document.getElementByID("maxMagBox")
+
+const querySettings = {
+    minMag : Math.max(0,document.getElementById("minMagBox").value), 
+    maxMag : Math.min(10,document.getElementById("maxMagBox").value)
+
+}
+// const minMag = Math.max(0,document.getElementById("minMagBox"))
+// const maxMag = Math.min(10,document.getElementById("maxMagBox"))
+// obj.features[0].properties
 
 
+// storing api call results in this object to iterate /referenc elater
 let obj;
 
-fetch("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02")
-  .then(response => response.json())
-  .then(data => obj = data)
-  .then(() => console.log(obj))
-  .then( () => {
-				for (let i = 0; i < obj.features.length; i++) {
-					console.log(obj.features[i].id);
-					console.log(obj.features[i].detail);
-				}
+// maybe can encapsulate these into a delivery object later
+let text = [];
+let mag = [];
+let lon = [];
+let lat = [];
+let size = [];
+let color = [];
 
-                render()
-			  }
-  );
+const markerScaleFac = 10;
+const magColors = ['#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026']
+
+function fetchApiDataAndRender() {
+    let apiQuery = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=${querySettings.minMag}`
+    fetch(apiQuery)
+    .then(response => response.json())
+    .then(data => obj = data)
+    .then(() => console.log(obj))
+    .then( () => {
+
+                  // populate the fields we need for plotting
+                  let innerHtmlString = "<tr><th>No.</th><th>Magnitude</th><th>Longitude</th><th>Lattitude</th><th>Place</th></tr>"
+                  for (let i = 0; i < obj.features.length; i++) {
+                      text.push(obj.features[i].properties.place);
+                      mag.push(obj.features[i].properties.mag);
+                      lon.push(obj.features[i].geometry.coordinates[0]);
+                      lat.push(obj.features[i].geometry.coordinates[1]);
+                      size.push(obj.features[i].properties.mag * markerScaleFac);
+                      color.push(assignColor(obj.features[i].properties.mag))
+                      
+                      innerHtmlString += `<tr>
+                      <td>${i}</td>
+                      <td>${mag[i]}</td>
+                      <td>${lon[i]}</td>
+                      <td>${lat[i]}</td>
+                      <td>${text[i]}</td>
+                      </tr>`;
+                  }
+
+                  // create a table for showing results
+
+                  document.getElementById('entriesTable').innerHTML = innerHtmlString;
+
+  
+                  renderMap()
+                }
+    );
+}
 
 
+function assignColor(magnitude) {
+    if (magnitude < 2) {
+        return magColors[0]
+    } else if (magnitude < 3) {
+        return magColors[1]
+    } else if (magnitude < 4) {
+        return magColors[2]
+    } else if (magnitude < 5) {
+        return magColors[3]
+    } else if (magnitude < 6) {
+        return magColors[4]
+    } else {
+        return magColors[5]
+    }
+}
 
-
-
-
-function render() {
-
+function renderMap() {
 
     var data = [{
         type: 'scattergeo',
         mode: 'markers+text',
-        text: [
-            'Montreal', 'Toronto', 'Vancouver', 'Calgary', 'Edmonton',
-            'Ottawa', 'Halifax', 'Victoria', 'Winnepeg', 'Regina'
-        ],
-        lon: [
-            -73.57, -79.24, -123.06, -114.1, -113.28,
-            -75.43, -63.57, -123.21, -97.13, -104.6
-        ],
-        lat: [
-            45.5, 43.4, 49.13, 51.1, 53.34, 45.24,
-            44.64, 48.25, 49.89, 50.45
-        ],
+        text: text, 
+        lon: lon, 
+        lat: lat, 
         marker: {
-            size: 7,
-            color: [
-                '#bebada', '#fdb462', '#fb8072', '#d9d9d9', '#bc80bd',
-                '#b3de69', '#8dd3c7', '#80b1d3', '#fccde5', '#ffffb3'
-            ],
+            size: size,
+            color: color,
             line: {
                 width: 1
             }
         },
-        name: 'Canadian cities',
+
+        name: 'Earthquakes',
         textposition: [
             'top right', 'top left', 'top center', 'bottom right', 'top right',
             'top left', 'bottom right', 'bottom left', 'top right', 'top right'
@@ -66,7 +113,7 @@ function render() {
         autosize: false, 
         width: 1000, 
         height: 1000,
-        title: 'Canadian cities',
+        title: 'Earthquakes',
         font: {
             family: 'Droid Serif, serif',
             size: 6
@@ -88,7 +135,7 @@ function render() {
             showlakes: true,
             lakecolor: '#fff',
             showland: true,
-            landcolor: '#EAEAAE',
+            landcolor: '#969696',
             countrycolor: '#d3d3d3',
             countrywidth: 1.5,
             subunitcolor: '#d3d3d3'
@@ -98,3 +145,6 @@ function render() {
     Plotly.newPlot('myDiv', data, layout);
 
 }
+
+
+fetchApiDataAndRender()
